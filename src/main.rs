5701927718de5983417ebcce08af089cb66c7a45
kevlarr <echo1+git@dvnl.net>
2018-11-24 22:22:59 -0500

    serve hello html

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,9 @@ fn handle(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
