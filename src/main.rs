51a65556486bc4b39b835fb3e6e92366b399883f
kevlarr <echo1+git@dvnl.net>
2018-11-24 22:23:16 -0500

    basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:1337").unwrap();
+    let addr = "127.0.0.1:1337";
+    let listener = TcpListener::bind(addr).unwrap();
+    println!("Now serving delicious HTTP on {}", addr);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
@@ -13,6 +16,7 @@ fn main() {
 fn handle(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
+    println!("Request:\n{}", String::from_utf8_lossy(&buffer));
 
     let contents = fs::read_to_string("hello.html").unwrap();
     let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
